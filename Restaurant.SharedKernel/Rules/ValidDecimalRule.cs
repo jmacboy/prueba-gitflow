9a6162a1c1e4b0cc16f5fae152a0680b981c12cc
jmacboy <j.m.alvarez.camacho@gmail.com>
2023-12-02 09:18:40 -0400

    agregado cambio en reglas de negocio

---
+++ Restaurant.SharedKernel/Rules/ValidDecimalRule.cs
@@ -0,0 +1,19 @@
+﻿using Restaurant.SharedKernel.Core;
+
+namespace Restaurant.SharedKernel.Rules;
+
+public class ValidDecimalRule : IBussinessRule
+{
+    private readonly decimal _value;
+
+    public string Message => "Value is not a valid decimal";
+
+    public ValidDecimalRule(decimal value)
+    {
+        _value = value;
+    }
+    public bool IsValid()
+    {
+        return _value >= 0;
+    }
+}
